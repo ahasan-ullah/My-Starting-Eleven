9d579fbfa2128fbf7f2010245f3c3ec523fd3cd8
Ahasan Ullah <ahasanullah451@gmail.com>
2024-10-25 20:59:45 +0600

    footer section modified

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,17 @@
-import NavbarLogo from '../assets/logo.png'
-const Footer=()=>{
+import NavbarLogo from "../assets/logo.png";
+const Footer = () => {
   return (
-    <div className='bg-[#06091A] p-5 md:p-10'>
+    <div className="bg-[#06091A] p-5 md:p-10">
       <div className="flex items-center justify-center md:mb-10">
         <img className="w-20 md:w-28" src={NavbarLogo} alt="" />
       </div>
       <footer className="footer">
         <nav>
           <h6 className="text-lg font-bold text-[#FFFFFF]">Services</h6>
-          <div className="text-[#FFFFFF99] text-justify">My Starting Eleven offers a dynamic fantasy sports <br/>platform where fans can create teams, compete, <br/>and earn rewards based on real player performances.
+          <div className="text-[#FFFFFF99] text-justify">
+            My Starting Eleven offers a dynamic fantasy sports <br />
+            platform where fans can create teams, compete, <br />
+            and earn rewards based on real player performances.
           </div>
         </nav>
         <nav>
@@ -22,7 +25,9 @@ const Footer=()=>{
           <h6 className="text-lg font-bold text-[#FFFFFF]">Subscribe</h6>
           <fieldset className="form-control w-full">
             <label className="label">
-              <span className="label-text text-[#FFFFFF99]">Subscribe to our newsletter for the latest updates.</span>
+              <span className="label-text text-[#FFFFFF99]">
+                Subscribe to our newsletter for the latest updates.
+              </span>
             </label>
             <div className="join">
               <input
@@ -30,11 +35,20 @@ const Footer=()=>{
                 placeholder="Enter your e-mail"
                 className="input input-bordered join-item w-48 md:w-full"
               />
-              <button onClick={(event)=>event.preventDefault()} className="btn join-item bg-gradient-to-r from-indigo-300 via-orange-300 to-pink-300">Subscribe</button>
+              <button
+                onClick={(event) => event.preventDefault()}
+                className="btn join-item bg-gradient-to-r from-indigo-300 via-orange-300 to-pink-300"
+              >
+                Subscribe
+              </button>
             </div>
           </fieldset>
         </form>
       </footer>
+      <div className="divider"></div>
+      <div>
+        <p className="text-center text-[#FFFFFF99] text-sm">@2024 My Starting Eleven All Rights Reserved.</p>
+      </div>
     </div>
   );
 };
